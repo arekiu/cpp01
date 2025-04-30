9cbe11b736de25306b888dbe560cbc1fd10a9630
arekiu <alex.isaguilera@hotmail.com>
2025-04-30 16:00:47 +0200

    finish ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string	&stringREF = brain;
+
+	std::cout << "Address of brain : " << &brain << std::endl;
+	std::cout << "Address held by stringPTR : " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of brain : " << brain << std::endl;
+	std::cout << "Value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
