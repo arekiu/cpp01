2345d0a0ab8d455b9af5b947f7541baae253dd42
arekiu <alex.isaguilera@hotmail.com>
2025-05-02 19:49:34 +0200

    define header FileChanger

---
+++ ex04/FileChanger.hpp
@@ -0,0 +1,15 @@
+#ifndef FILECHANGER_HPP
+#define FILECHANGER_HPP
+
+#include <string>
+
+class FileChanger {
+	private:
+			std::string	_filename;
+			std::string	_s1;
+			std::string	_s2;
+
+	public:
+};
+
+#endif
