21eabfa679cf96527ca67ee93b7e0c9ccf51b948
arekiu <alex.isaguilera@hotmail.com>
2025-05-03 06:57:11 +0200

    initialize program, get arguments

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,7 +1,24 @@
+#include "FileChanger.hpp"
 #include <iostream>
-#include <string>
 
-int	main(void)
-{
+int	main(int argc, char* argv[]) {
+
+	if (argc != 4)
+	{
+		std::cerr << "Invalidad number of arguments.\n\
+		Usage: ./<program> <filename> <s1> <s2>" << std::endl;
+		return (1);
+	}
+
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	if (s1.empty()) {
+		std::cerr << "Error: s1 cannot be empty." << std::endl;
+		return 1;
+	}
+
+	FileChanger changer(filename, s1, s2);
 
 }
